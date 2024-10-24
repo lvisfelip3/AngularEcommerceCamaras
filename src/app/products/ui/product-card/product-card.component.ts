35cdd616f46059a1bce266b30bbaf394e5e44c63
lvisfelip3 <luisfeipe.vera23@gmail.com>
2024-10-24 11:47:40 -0300

    Se ajusta el layout y diseño del catalogo y sus cards con productos.

--- src/app/products/ui/product-card/product-card.component.ts
+++ src/app/products/ui/product-card/product-card.component.ts
@@ -3,11 +3,17 @@ import { Product } from '../../../shared/interfaces/interfaces';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatIconModule, RouterLink, CurrencyPipe],
+  imports: [
+    MatIconModule, 
+    RouterLink, 
+    CurrencyPipe,
+    MatButtonModule
+  ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })

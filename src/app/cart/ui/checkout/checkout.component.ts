29c90548b4bec4559bc21232e4e2096aee06f226
lvisfelip3 <luisfeipe.vera23@gmail.com>
2024-10-24 11:47:17 -0300

    Se ajusta el layout del cart component

--- src/app/cart/ui/checkout/checkout.component.ts
+++ src/app/cart/ui/checkout/checkout.component.ts
@@ -2,11 +2,16 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartStateService } from '../../../shared/data-access/cart-state.service';
 import { CurrencyPipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [RouterLink, CurrencyPipe],
+  imports: [
+    RouterLink, 
+    CurrencyPipe,
+    MatButtonModule
+  ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })

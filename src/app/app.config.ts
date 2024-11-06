42a423786d4679df64a5599ae97f8a10f19e557e
lvisfelip3 <luisfeipe.vera23@gmail.com>
2024-11-06 16:42:48 -0300

    Fix: ssr to csr and various bug fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideClientHydration } from '@angular/platform-browser';
+// import { provideClientHydration } from '@angular/platform-browser'; only with SSR
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withFetch()), provideAnimationsAsync(), provideClientHydration(),
+    provideHttpClient(withFetch()), provideAnimationsAsync(),
     provideExperimentalZonelessChangeDetection(),
+    // provideClientHydration(), only with SSR
   ]
 };

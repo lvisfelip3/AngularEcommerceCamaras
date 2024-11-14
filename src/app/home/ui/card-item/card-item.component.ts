8a31a589194322213f74d54aab398c2b7ac0f75f
lvisfelip3 <luisfeipe.vera23@gmail.com>
2024-11-14 17:26:42 -0300

    Fix: Minor Bug fixes

--- src/app/home/ui/card-item/card-item.component.ts
+++ src/app/home/ui/card-item/card-item.component.ts
@@ -1,8 +1,12 @@
 import { CurrencyPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input} from '@angular/core';
 import { Product } from '@shared/interfaces/interfaces';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CartStateService } from '@shared/data-access/cart-state.service';
+import { FavoriteStateService } from '@shared/data-access/fav-state.service';
+import { SnackBarService } from '@shared/ui/snack-bar.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card-item',
@@ -10,7 +14,8 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CurrencyPipe,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterLink
   ],
   templateUrl: './card-item.component.html',
   styleUrl: './card-item.component.css',
@@ -18,4 +23,21 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class CardItemComponent {
   product = input.required<Product>()
+  cartState = inject(CartStateService).state
+  favState = inject(FavoriteStateService).state
+  private readonly _snackBar = inject(SnackBarService)
+
+  addToCart(e: Event) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.cartState.add({ product: this.product(), quantity: 1 });
+    this._snackBar.showSnackBar('Producto agregado', 'OK');
+  }
+
+  addToFav(e: Event) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.favState.add(this.product());
+    this._snackBar.showSnackBar('Producto agregado a favoritos', 'OK');
+  }
 }

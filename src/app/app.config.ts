556837e76d413844ac646ebe0efc8756b58e1c23
lvisfelip3 <luisfeipe.vera23@gmail.com>
2024-11-28 23:47:36 -0300

    Fix: Added a config where it reset the scroll between the pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,9 +6,19 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 // import { provideClientHydration } from '@angular/platform-browser'; only with SSR
 
+import { InMemoryScrollingFeature, InMemoryScrollingOptions, withInMemoryScrolling } from '@angular/router';
+
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), inMemoryScrollingFeature),
     provideHttpClient(withFetch()), provideAnimationsAsync(),
     provideExperimentalZonelessChangeDetection(),
     // provideClientHydration(), only with SSR

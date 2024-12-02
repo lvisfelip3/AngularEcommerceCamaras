872b8ea09213ad9608476985b96533ac1e47115f
lvisfelip3 <luisfeipe.vera23@gmail.com>
2024-12-02 20:46:04 -0300

    Feature: now the admin mainteiner of products acept webp image files.

--- src/app/admin/maintainers/products/dialog-product.component.ts
+++ src/app/admin/maintainers/products/dialog-product.component.ts
@@ -76,7 +76,7 @@ import { CategoriasService } from '../category/category.service';
               class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
               type="file"
               (change)="onChangeImage($event)"
-              accept="image/png, image/jpeg"
+              accept="image/png, image/jpeg, image/webp"
             />
           </label>
           @if (imagePreview) {

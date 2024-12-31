7e8876d98f814a4073ef63ea9fc1788729985dab
lvisfelip3 <luisfeipe.vera23@gmail.com>
2024-12-31 19:01:51 -0300

    Feature: Enhance product dialog with loading spinner and improved image display

--- src/app/admin/maintainers/products/dialog-product-image.component.ts
+++ src/app/admin/maintainers/products/dialog-product-image.component.ts
@@ -6,22 +6,33 @@ import {
 } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { Product } from '../../../shared/interfaces/interfaces';
+import { Product } from '@shared/interfaces/interfaces';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-product-dialog',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatSelectModule],
+  imports: [
+    MatDialogModule, 
+    MatButtonModule, 
+    MatInputModule, 
+    MatSelectModule,
+    MatProgressSpinnerModule
+  ],
   template: `
     <div class="dialog">
       <h1 mat-dialog-title class="title">
         {{ data.nombre }}
       </h1>
       <div mat-dialog-content>
-        <div class="rounded-lg">
-          <img src="{{ data.imagen }}" alt="imagen producto" class="w-full h-auto flex justify-center items-center px-5"/>
-        </div>
+        @defer{
+          <img src="{{ data.imagen }}" alt="producto: {{ data.nombre }}" class="w-full h-96 flex justify-center items-center rounded-lg object-cover"/>
+        } @loading{
+          <div class="flex justify-center items-center">
+            <mat-spinner mode="indeterminate"></mat-spinner>
+          </div>
+        }
       </div>
       <div mat-dialog-actions class="footer">
         <button mat-button (click)="onCancel()">Cerrar</button>
@@ -54,7 +65,7 @@ import { MatSelectModule } from '@angular/material/select';
       }
 
       .dialog {
-        @apply bg-slate-700 !important;
+        @apply !bg-slate-700 !p-3 md:!p-4 !min-h-96;
       }
 
       :host ::ng-deep mat-dialog-content {

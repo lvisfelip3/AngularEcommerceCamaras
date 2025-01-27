24f4d89caff0d490928f12fd41b181018091b13e
lvisfelip3 <luisfeipe.vera23@gmail.com>
2025-01-27 18:12:07 -0300

    feat: enhance card item skeleton and component layout for improved visual consistency

--- src/app/home/ui/card-item/card-item-skeleton.component.ts
+++ src/app/home/ui/card-item/card-item-skeleton.component.ts
@@ -8,7 +8,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   template: `<article class="flex flex-col w-max h-auto border shadow-lg dark:shadow-none border-gray-300 dark:border-gray-600 rounded-lg relative animate-pulse">
   <div class="h-40 w-52 max-w-52 md:h-56 md:w-64 md:max-w-64 bg-gray-300 dark:bg-gray-500 rounded-t-md border-b border-gray-300 dark:border-gray-600"></div>
   <div class="p-4">
-    <div class="h-4 bg-gray-300 dark:bg-gray-500 rounded w-3/5 mb-4"></div>
+    <div class="h-14 bg-gray-200 dark:bg-gray-700 rounded w-full mb-4"></div>
     <div class="flex justify-between items-center">
         <div class="h-5 bg-gray-300 dark:bg-gray-500 rounded w-16"></div>
         <div class="flex gap-2">
